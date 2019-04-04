9be4cdfe5f021cb51be8d90c9a314723c5e00d3d
diegosoriarios <diegosoriarios@gmail.com>
2019-04-04 18:31:28 -0300

    Check Network Connection

--- app/src/main/java/com/example/diegorios/webview/MainActivity.kt
+++ app/src/main/java/com/example/diegorios/webview/MainActivity.kt
@@ -5,21 +5,35 @@ import android.os.Bundle
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import com.androindian.raj.imei.CheckNet
 
 class MainActivity : AppCompatActivity() {
 
     var mywebview: WebView? = null
 
+    lateinit var cd: CheckNet
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mywebview = findViewById<WebView>(R.id.webview)
-        mywebview!!.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url)
-                return true
+
+        cd = CheckNet()
+        if (cd.isConnectingToInternet(this@MainActivity)) {
+            setContentView(R.layout.activity_main)
+            mywebview = findViewById<WebView>(R.id.webview)
+            mywebview!!.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    view?.loadUrl(url)
+                    return true
+                }
             }
+            mywebview!!.loadUrl("https://diegosoriarios.github.io")
+        } else {
+            Toast.makeText(applicationContext,
+                "no net", Toast.LENGTH_LONG).show()
         }
-        mywebview!!.loadUrl("https://diegosoriarios.github.io")
+
     }
+
 }
