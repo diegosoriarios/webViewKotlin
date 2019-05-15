8a08c3f6143b3b65370e8d9d3c85a3912cdbe2b1
diegosoriarios <diegosoriarios@gmail.com>
2019-05-14 22:16:06 -0300

    Splashscreen

--- app/src/main/java/com/example/diegorios/webview/MainActivity.kt
+++ app/src/main/java/com/example/diegorios/webview/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.diegorios.webview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebResourceRequest
+import android.webkit.SslErrorHandler
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import com.androindian.raj.imei.CheckNet
+import kotlinx.android.synthetic.main.activity_main.*
+import android.net.http.SslError
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +32,10 @@ class MainActivity : AppCompatActivity() {
                     return true
                 }
             }
-            mywebview!!.loadUrl("https://diegosoriarios.github.io")
+
+            mywebview!!.settings.javaScriptEnabled = true
+            mywebview!!.loadUrl("https://admin:admin@ccstore-prod-zdoa.oracleoutsourcing.com")
+
         } else {
             Toast.makeText(applicationContext,
                 "no net", Toast.LENGTH_LONG).show()
